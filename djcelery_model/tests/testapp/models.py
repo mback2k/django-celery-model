80d5da8fec4a10df937a1b7e10cbcf0a6ee0097f
Gordon Pendleton <wgordonw1@gmail.com>
2019-11-10 18:22:33 -0500

    make migration

--- djcelery_model/tests/testapp/models.py
+++ djcelery_model/tests/testapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
+from djcelery_model.models import TaskMixin
 
-class JPEGFile(models.Model):
+class JPEGFile(TaskMixin, models.Model):
     file = models.FileField()
     etag = models.CharField(max_length=255)
