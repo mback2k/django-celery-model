8a39e37114eb97f3e4aedbf72a0fc8c87bc7576d
Gordon Pendleton <wgordonw1@gmail.com>
2020-07-13 14:24:05 -0400

    tested relating same task across multiple instances and models

--- djcelery_model/tests/base.py
+++ djcelery_model/tests/base.py
@@ -6,6 +6,7 @@ from djcelery_model.tests import celery_app
 
 class CeleryTestCase(TransactionTestCase):
     def setUp(self):
+        super().setUp()
         self.worker_context = start_worker(celery_app, perform_ping_check=False)
         self.worker = self.worker_context.__enter__()
         self.worker.ensure_started()
--- djcelery_model/tests/test_models.py
+++ djcelery_model/tests/test_models.py
@@ -1,16 +1,25 @@
+import time
+
 from django.contrib.sites.models import Site
-from django.contrib.contenttypes.models import ContentType
 from django.db import IntegrityError
 from django.test import TestCase
+from celery.contrib.testing.tasks import ping
 
-from ..models import ModelTaskMeta
+from ..models import ModelTaskMeta, ModelTaskMetaState
+from .base import CeleryTestCase
+from .testapp.models import JPEGFile
 
 
-class ModelTaskMetaTests(TestCase):
+class SetUpMixin(object):
     def setUp(self):
+        super().setUp()
         self.site_a = Site.objects.create(domain='www.a.com', name='a.com')
         self.site_b = Site.objects.create(domain='www.b.com', name='b.com')
-    
+        self.record = JPEGFile.objects.create(pk=self.site_a.pk)
+        self.instances = (self.site_a, self.site_b, self.record)
+
+
+class ModelTaskMetaTests(SetUpMixin, TestCase):
     def test_task_id_unique_per_relation(self):
         ModelTaskMeta.objects.create(content_object=self.site_a, task_id='foo')
         with self.assertRaises(IntegrityError):
@@ -22,5 +31,26 @@ class ModelTaskMetaTests(TestCase):
     
     def test_task_id_not_unique_per_relation_id(self):
         ModelTaskMeta.objects.create(content_object=self.site_a, task_id='foo')
-        record = ContentType.objects.get(pk=self.site_a.pk)
-        ModelTaskMeta.objects.create(content_object=record, task_id='foo')
+        ModelTaskMeta.objects.create(content_object=self.record, task_id='foo')
+
+
+class MultiModelStateUpdateTests(SetUpMixin, CeleryTestCase):
+    def test_ping_across_models(self):
+        task_id = 'c51352f0-cf93-4781-9288-ae5e4a362648'
+        metas = (
+            ModelTaskMeta.objects.create(content_object=self.site_a, task_id=task_id),
+            ModelTaskMeta.objects.create(content_object=self.site_b, task_id=task_id),
+            ModelTaskMeta.objects.create(content_object=self.record, task_id=task_id),
+        )
+        result = ping.apply_async(task_id=task_id)
+        time.sleep(1)
+        self.assertTrue(result.ready())
+        self.assertTrue(result.successful())
+        for meta in metas:
+            meta.refresh_from_db()
+        self.assertEqual(metas[0].state, ModelTaskMetaState.SUCCESS)
+        self.assertEqual(metas[1].state, ModelTaskMetaState.SUCCESS)
+        self.assertEqual(metas[2].state, ModelTaskMetaState.SUCCESS)
+        self.assertEqual(3, ModelTaskMeta.objects.filter(task_id=task_id).count())
+        metas[0].result.forget()
+        self.assertEqual(0, ModelTaskMeta.objects.filter(task_id=task_id).count())
